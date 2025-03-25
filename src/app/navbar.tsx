fdd882d2e57b4ea04e6286afc86c6e0183404c34
yadiOs-a-darel <alphonsodarely@gmail.com>
2025-03-25 13:38:29 +0100

    three commit

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,27 +1,16 @@
 
 import Link from "next/link";
-import { useState } from "react";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { RxCross1 } from "react-icons/rx";
 
 
 export default function NavBar() {
 
-  const [isOpen, setIsOpen] = useState(false);
-
-    <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="z-20 sm:hidden"
-        >
-          {isOpen ? <RxCross1 /> : <HiMenuAlt4 />}
-      </button>
+  
   return (
     <div className=" text-black bg-white">
-        <nav className="bg-white border-gray-200 ">
-          <div className="max-w-screen-xl flex items-center  justify-between mx-auto p-4">
-            <div className={`fixed inset-0 bg-black items-center justify-between ${ isOpen ? 'flex' : 'hidden sm:flex'} 
-              hidden w-full md:flex md:w-auto md:order-1 " id="navbar-cta`}>
+        <nav className="bg-white border-gray-200 fixed inset-0 sm:relative">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 " id="navbar-cta">
                 <ul className="flex flex-col mt-4  rtl:space-x-reverse md:flex-row gap-8 ">
                   <li>
                     <Link href="/" aria-current="page"></Link>
